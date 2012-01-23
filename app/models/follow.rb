bb969300288e42f3d74591b997bcdebca31acf31
Fabiano Almeida <fabianoalmeida@gmail.com>
2012-01-23 13:38:13 -0300

    Renamed 'user' to 'follower' and 'follower' to 'followed'

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,12 +1,6 @@
 class Follow < ActiveRecord::Base
   
-  belongs_to :user
-  belongs_to :follower, :class_name => "User", :foreign_key => "follower_id"
-  
-  # callback
-  after_destroy do |f|
-    User.decrement_counter(:following_count, f.user_id)
-    User.decrement_counter(:followers_count, f.follower_id)
-  end
+  belongs_to :follower, :class_name => "User", :foreign_key => "user_id"
+  belongs_to :followed, :class_name => "User", :foreign_key => "followed_id"
   
 end
