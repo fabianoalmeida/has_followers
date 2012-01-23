0107060e892b7ad2dc557c639337e90f9ee3073b
Fabiano Almeida <fabianoalmeida@gmail.com>
2012-01-23 13:41:16 -0300

    Changed the references name to followed and its index name and user's counter

--- lib/generators/has_followers/migration/templates/create_has_followers_tables.rb
+++ lib/generators/has_followers/migration/templates/create_has_followers_tables.rb
@@ -1,20 +1,20 @@
-class CreateHasFriendsTables < ActiveRecord::Migration
+class CreateHasFollowersTables < ActiveRecord::Migration
   def self.up
     create_table :follows, :force => true do |t|
-      t.references :user, :follower
+      t.references :followed, :user
       t.timestamps
     end
     
+    add_index :follows, :followed_id
     add_index :follows, :user_id
-    add_index :follows, :follower_id
     
     add_column :users, :followers_count, :integer, :default => 0, :null => false
-    add_column :users, :following_count, :integer, :default => 0, :null => false
+    add_column :users, :followeds_count, :integer, :default => 0, :null => false
   end
 
   def self.down
     remove_column :users, :followers_count
-    remove_column :users, :following_count
+    remove_column :users, :followeds_count
     
     drop_table :follows
   end
